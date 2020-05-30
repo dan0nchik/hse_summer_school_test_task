665308a6e5408f05adf85f3d852708a943c0d148
Daniel Khromov <danielkhromov@gmail.com>
2020-05-30 12:21:49 +0300

    Initial Commit

---
+++ hse_summer_school_test_task/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hse_summer_school_test_task
+//
+//  Created by Daniel Khromov on 5/30/20.
+//  Copyright © 2020 Daniel Khromov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
